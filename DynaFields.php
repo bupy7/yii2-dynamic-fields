5d58f4eb82b6e353cc422f289f79b527272fc18c
Vasilij Belosludcev <bupy765@gmail.com>
2015-08-24 13:59:32 +0500

    added js validation

--- DynaFields.php
+++ DynaFields.php
@@ -7,6 +7,8 @@ use yii\base\Widget;
 use yii\base\InvalidConfigException;
 use yii\base\Model;
 use yii\widgets\Pjax;
+use yii\helpers\Json;
+use yii\web\View;
 
 /**
  * Widget for display dynamic fields, adding and removing their use Pjax.
@@ -18,66 +20,54 @@ use yii\widgets\Pjax;
  */
 class DynaFields extends Widget
 {
-    
     /**
      * @var ActiveForm the form that this field is associated with.
      */
     public $form;
-    
     /**
      * @var array Options of the 'field' method.
      * @see \yii\widgets\ActiveForm::field()
      */
     public $fieldOptions = [];
-    
     /**
      * @var string Name of input method from \yii\widgets\ActiveField. By default 'textInput'.
      * @see \yii\widgets\ActiveField
      */
     public $inputMethod = 'textInput';
-    
     /**
      * @var array Arguments of current $inputMethod as array. First argument is [0], second is [1] and etc.
      * Example: By default $inputMehod is 'textInput'. Then argemnts can be: [['maxlength' => true]].
      */
     public $inputMethodArgs = [];
-    
     /**
      * @var array Models the data model that this widget is associated with.
      */
     public $models;
-    
     /**
      * @var string Primary key of model. By default 'id'.
      */
     public $primaryKey = 'id';
-    
     /**
      * @var string the model attribute that this widget is associated with.
      */
     public $attribute;
-    
     /**
      * @var mixed URL of action for create new model.
      */
     public $urlAdd;
-    
     /**
      * @var mixed URL of action for delete model.
      */
     public $urlRemove;
-    
     /**
      * @var array Options of action button.
      */
     public $buttonOptions = ['class' => 'btn btn-default'];
-    
     /**
      * @var string Template of input. List allow tokens: {input} and {button}. In token {button} will be inserted 
      * action button. In token {input} will be inserted input field.
      */
     public $inputTemplate = '<div class="input-group">{input}<span class="input-group-btn">{button}</span></div>';
-    
     /**
      * @var array Options of Pjax.
      * @see \yii\widgets\Pjax
@@ -88,17 +78,17 @@ class DynaFields extends Widget
             'type' => 'post',
         ],
     ];
-    
     /**
      * @var boolean Whether set 'true' then will be displays label for each field and not only for first field.
      */
     public $labelEach = false;
-    
     /**
      * @var boolean Whether set 'true' then will be displays hint for each field and not only for first field.
      */
     public $hintEach = false;
     
+    private $_pjax;
+
     /**
      * @inheritdoc
      */
@@ -110,9 +100,9 @@ class DynaFields extends Widget
         if (empty($this->urlAdd) || empty($this->urlRemove)) {
             throw new InvalidConfigException("Either 'urlAdd' and 'urlRemove' properties must be specified.");
         }
-        Pjax::begin($this->pjaxOptions);
+        $this->_pjax = Pjax::begin($this->pjaxOptions);
     }
-    
+
     /**
      * @inheritdoc
      */
@@ -130,23 +120,17 @@ class DynaFields extends Widget
         $button = Html::a(
             Html::tag('span', '', [
                 'class' => 'glyphicon glyphicon-plus',
-            ]), 
-            $this->urlAdd, 
-            $this->buttonOptions
+            ]), $this->urlAdd, $this->buttonOptions
         );
         echo $this->field($form, $this->models[$keys[0]], "[{$keys[0]}]{$this->attribute}", $button);
         if (!$this->labelEach) {
             $form->fieldConfig['template'] = str_replace(
-                '{label}', 
-                Html::tag('label', '', $form->fieldConfig['labelOptions']), 
-                $form->fieldConfig['template']
+                '{label}', Html::tag('label', '', $form->fieldConfig['labelOptions']), $form->fieldConfig['template']
             );
         }
         if (!$this->hintEach) {
             $form->fieldConfig['template'] = preg_replace(
-                '/\{hint\}|\{hint\}\\n|\{hint\}\s/i',
-                '', 
-                $form->fieldConfig['template']
+                '/\{hint\}|\{hint\}\\n|\{hint\}\s/i', '', $form->fieldConfig['template']
             );
         }
         for ($i = 1; $i != count($keys); $i++) {
@@ -154,15 +138,35 @@ class DynaFields extends Widget
                 Html::tag('span', '', [
                     'class' => 'glyphicon glyphicon-minus',
                 ]), 
-                array_merge((array)$this->urlRemove, ['id' => $this->models[$i]->{$this->primaryKey}]),
+                array_merge((array) $this->urlRemove, [
+                    'id' => $this->models[$i]->{$this->primaryKey},
+                ]), 
                 $this->buttonOptions
             );
             echo $this->field($form, $this->models[$keys[$i]], "[{$keys[$i]}]{$this->attribute}", $button);
-        }      
-        $this->form->attributes = $form->attributes;
+        }
+        if ($this->form->enableClientScript) {
+            $clientOptions = [];
+            for ($i = 0; $i != count($form->attributes); $i++) {
+                if (strpos($form->attributes[$i]['name'], $this->attribute) !== false) {
+                    $clientOptions[] = $form->attributes[$i];
+                }
+            }
+            $clientOptions = Json::encode($clientOptions);
+            $js = <<<JS
+(function($) {
+    var clientOptions = {$clientOptions},
+        \$form = $('#{$this->form->id}');
+    for (var i = 0; i != clientOptions.length; i++) {
+        \$form.yiiActiveForm('add', clientOptions[i]);
+    }
+}(jQuery));
+JS;
+            $this->view->registerJs($js, View::POS_LOAD);
+        }
         Pjax::end();
     }
-    
+
     /**
      * Render field of \yii\widgets\ActiveForm.
      * @param \yii\widgets\ActiveForm $form
@@ -174,10 +178,10 @@ class DynaFields extends Widget
     protected function field($form, $model, $attribute, $button)
     {
         $field = $form->field($model, $attribute, $this->fieldOptions);
-        $field = call_user_func_array([$field, $this->inputMethod], $this->inputMethodArgs);  
-        return str_replace('{button}', $button, $field);   
+        $field = call_user_func_array([$field, $this->inputMethod], $this->inputMethodArgs);
+        return str_replace('{button}', $button, $field);
     }
-    
+
     /**
      * @return boolean whether this widget is associated with a data model.
      */
@@ -194,5 +198,5 @@ class DynaFields extends Widget
         }
         return true;
     }
-    
+
 }
